53a55d04c6038764e8c2e4744ec190e35be682d7
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-28 14:10:02 +0300

    finish the part one step 4

--- toolkit.py
+++ toolkit.py
@@ -12,3 +12,8 @@ def square(n):
     print(f"the square {n}** {n**2}")
 square(5)
 square(12)
+# part 4
+def greet_with_title(name, title="Agent"):
+    print(f"Hello, {name} {title}")
+greet_with_title(name = "yossi")
+greet_with_title("shneor", "solder")
