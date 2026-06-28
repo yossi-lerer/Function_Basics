b3dcab631cd18ab55193e3adc779d61e6c1349e0
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-28 14:44:25 +0300

    finish the part one step 8

--- toolkit.py
+++ toolkit.py
@@ -3,27 +3,27 @@
 def greet(name):
     print(f"Hello {name}")
 greet("Agent X")
-# part 2
+# step 2
 def add(a, b):
     print(f"the result of {a} + {b} is {a + b}")
 add(3, 4)
-# part 3
+# step 3
 def square(n):
     print(f"the square {n}** {n**2}")
 square(5)
 square(12)
-# part 4
+# step 4
 def greet_with_title(name, title="Agent"):
     print(f"Hello, {title} {name}")
 greet_with_title(name = "yossi")
 greet_with_title("Gad", "King")
-# part 5
+# step 5
 def describe(name, level, active):
     print(f"Name: {name}")
     print(f"Level: {level}")
     print(f"Active: {active}")
 describe(level=7, name="yossi", active=True) 
-# part 6
+# step 6
 def multiply(a, b=2):
     print(f"The result of {a} * {b} is {a * b}")
 multiply(5)
@@ -38,3 +38,9 @@ def print_largest(a, b, c):
 print_largest(3,8,5)
 print_largest(10,2,7)
 print_largest(4,4,1)
+# step 8
+def show_fahrenheit(c):
+    print(f"the Celsius convet to Fahrenheit is {(c * 9 / 5) + 32}")
+show_fahrenheit(0)
+show_fahrenheit(100)
+show_fahrenheit(37.5)
